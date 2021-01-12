6fbcaba5c3e6a11069492387ac8ab50f3238ba30
dunnyk <danielkamau231@gmail.com>
2021-01-12 23:40:25 +0300

    latest update

---
+++ awesome_website/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from core import views as core_views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", core_views.listing, name="listing"),
+    path("view_blog/<int:blog_id>/", core_views.view_blog, name="view_blog"),
+]
---
+++ awesome_website/core/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from core.models import Blog
+
+# Create your views here.
+
+def listing(request):
+    data = {
+        "blog": Blog.objects.all(),
+    }
+    return render(request, "listing.html", data)
+
+def view_blog(request, blog_id):
+    blog = get_object_or_404(Blog, blog_id)
+    data = {
+        "blog": blog,
+    }
+    return render(request, "view_blog.html", data)
